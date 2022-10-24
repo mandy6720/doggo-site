2b5194e21a47f20220819f6cda97ba6060825e07
Amanda Bozzi <amanda.hewitt01@gmail.com>
2022-10-24 14:06:03 -0500

    DARk MODE YASSSS!

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import "../styles/global.scss";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
