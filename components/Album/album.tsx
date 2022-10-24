cc216e57c475bba0c097d8a8f25d62cbbcafeb60
Amanda Bozzi <amanda.hewitt01@gmail.com>
2022-10-24 12:15:25 -0500

    get rid of some shit

--- components/Album/album.tsx
+++ components/Album/album.tsx
@@ -29,7 +29,7 @@ function Copyright() {
   );
 }
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const cards = [1, 2, 3, 4, 5, 6];
 
 const theme = createTheme();
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "../styles/global.scss";
+import { AppBar } from "@mui/material";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <AppBar position="static" />
+      <Component {...pageProps} />
+    </>
+  );
 }
