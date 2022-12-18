00dd1b94d03abf063579a28b7002fadce906ab86
mandy6720 <amanda.hewitt01@gmail.com>
2022-12-18 09:09:51 -0600

    updates appbar fonts

---
+++ components/Sections/about.js
@@ -0,0 +1,10 @@
+const About  = (name=dog, ltr=true ) => {
+  return (<section><h1 id="about">About Us</h1>
+  <span> "Our goal as a professional breeder is simple, to deliver American Dobermans with the highest standard of quality to new 
+  owners. In order to make such a commitment in raising pups for new homes, we made a promise. The promise made is to ensure that 
+  with love and compassion, these pups will be properly raised. The responsibility of bringing fragile lives into the world is a 
+  lifelong commitment from the Odremans. As we graduate a puppy from the litter to new families, we make it our MISSION to always 
+  be a single solution resource to inform, guide, and teach new members of our family!"</span></section>)
+}
+
+export default About
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,15 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
+import About from "../components/Sections/about"
 
-export default function Home () {
+export default function Home (children) {
   return (
     <Layout>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className="container">
-      <h1 id="about">About Us</h1>
-      <span> "Our goal as a professional breeder is simple, to deliver American Dobermans with the highest standard of quality to new 
-      owners. In order to make such a commitment in raising pups for new homes, we made a promise. The promise made is to ensure that 
-      with love and compassion, these pups will be properly raised. The responsibility of bringing fragile lives into the world is a 
-      lifelong commitment from the Odremans. As we graduate a puppy from the litter to new families, we make it our MISSION to always 
-      be a single solution resource to inform, guide, and teach new members of our family!"</span>
+        <About />
       </section>
     </Layout>
   );
