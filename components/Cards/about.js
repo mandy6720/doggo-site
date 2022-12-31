f8ad131548c2b76f7607eee5f9fa94e35d750472
mandy6720 <amanda.hewitt01@gmail.com>
2022-12-31 11:00:48 -0600

    B-E-A-utiful

--- components/Cards/about.js
+++ components/Cards/about.js
@@ -6,21 +6,23 @@ const Card = (name, ltr = true) => {
       <Box component="h2">
         <h1 id="about">About Us</h1>
       </Box>
-      <Avatar
-        variant="circular"
-        src="images/genericdobie.png"
-        sx={{ flex: 1, width: 200, height: 200 }}
-      />
-      <Box sx={{ paddingTop: "16px" }}>
-        "Our goal as a professional breeder is simple, to deliver American
-        Dobermans with the highest standard of quality to new owners. In order
-        to make such a commitment in raising pups for new homes, we made a
-        promise. The promise made is to ensure that with love and compassion,
-        these pups will be properly raised. The responsibility of bringing
-        fragile lives into the world is a lifelong commitment from the Odremans.
-        As we graduate a puppy from the litter to new families, we make it our
-        MISSION to always be a single solution resource to inform, guide, and
-        teach new members of our family!"
+      <Box sx={{ display: "flex" }}>
+        <Avatar
+          variant="circular"
+          src="images/genericdobie.png"
+          sx={{ width: 200, height: 200 }}
+        />
+        <Box sx={{ paddingLeft: "16px" }}>
+          "Our goal as a professional breeder is simple, to deliver American
+          Dobermans with the highest standard of quality to new owners. In order
+          to make such a commitment in raising pups for new homes, we made a
+          promise. The promise made is to ensure that with love and compassion,
+          these pups will be properly raised. The responsibility of bringing
+          fragile lives into the world is a lifelong commitment from the
+          Odremans. As we graduate a puppy from the litter to new families, we
+          make it our MISSION to always be a single solution resource to inform,
+          guide, and teach new members of our family!"
+        </Box>
       </Box>
     </Box>
   );
