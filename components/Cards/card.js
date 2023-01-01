13e2beda5930d7db3277c0f8b28c2e90daaa4eb8
mandy6720 <amanda.hewitt01@gmail.com>
2022-12-31 19:01:20 -0600

    generic card

--- components/Cards/card.js
+++ components/Cards/card.js
@@ -2,14 +2,14 @@ import { Box, Avatar } from "@mui/material";
 
 const Card = (name, ltr = true, text, img) => {
   return (
-    <Box id="about" className="card" spacing={2}>
+    <Box id={name} className="card" spacing={2}>
       <Box component="h2" variant="h4">
         <h2 id={name}>{name}</h2>
       </Box>
       <Box sx={{ display: "flex" }}>
         <Avatar
           variant="circular"
-          src="images/genericdobie.png"
+          src={img || "images/genericdobie.png"}
           sx={{ width: 200, height: 200 }}
         />
         {text}
