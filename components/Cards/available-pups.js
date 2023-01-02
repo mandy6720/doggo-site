fc0907e81bf808836c6d91318178925bb44de479
mandy6720 <amanda.hewitt01@gmail.com>
2023-01-02 13:49:46 -0600

    Adds some more setions

--- components/Cards/available-pups.js
+++ components/Cards/available-pups.js
@@ -1,7 +1,15 @@
+import { Typography, Box } from "@mui/material";
 import React from "react";
 
 const AvailablePups = () => {
-  return "hiyoo";
+  return (
+    <Box id="ourdogs" className="card" spacing={2}>
+      <Typography id="availablepups">
+        <h1>Available Pups</h1>
+        We ain't got none
+      </Typography>
+    </Box>
+  );
 };
 
 export default AvailablePups;
