722349c15569e52b8e240f550d124564b9ebe031
mandy6720 <amanda.hewitt01@gmail.com>
2023-01-04 04:06:46 -0600

    Centering of fleek

--- components/Blockquote.js/index.js
+++ components/Blockquote.js/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
+import { Flare } from "@mui/icons-material";
 
 const Blockquote = (text) => {
   return (
     <Box
       padding={2}
-      sx={{ textAlign: "center", marginTop: "1em", width: "80%" }}
+      sx={{
+        textAlign: "center",
+        marginTop: "1em",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+      }}
     >
       <Typography>
         <h2>
