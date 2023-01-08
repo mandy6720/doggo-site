bfce68a476d4d88acee1da84eb13f08ab0052971
mandy6720 <amanda.hewitt01@gmail.com>
2023-01-08 08:22:48 -0600

    More style fixes!

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -17,6 +17,7 @@ export default function Layout({ children }) {
           <meta name="description" content={`Information about ${siteTitle}`} />
           <meta
             property="og:image"
+            rel="preload"
             content={`https://og-image.vercel.app/${encodeURI(
               siteTitle
             )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
