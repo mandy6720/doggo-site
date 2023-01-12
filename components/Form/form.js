2dfca0ba347356edb042384d6a56449ab4c65b0d
mandy6720 <amanda.hewitt01@gmail.com>
2023-01-12 12:47:46 -0600

    form workinnnn

--- components/Form/form.js
+++ components/Form/form.js
@@ -5,6 +5,8 @@ import CssBaseline from "@mui/material/CssBaseline";
 
 const theme = createTheme();
 
+const handleClick = () => console.log("hi dad)");
+
 const Form = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -17,18 +19,25 @@ const Form = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main">
-        <CssBaseline />
+      <Container component="form">
         <Grid container>
           <Grid item marginRight={2}>
             <TextField id="name" label="name" type="text" />
           </Grid>
-          <Grid item marginRight={2}>
+          <Grid item marginRight={2} marginBottom={5}>
             <TextField id="email" label="email" type="email" marginRight={2} />
           </Grid>{" "}
-          <Grid item marginRight={2}>
+          <Grid item marginRight={2} marginBottom={5}>
             <TextField type="tel" label="phone" id="phone" marginRight={2} />
           </Grid>
+        </Grid>
+        <Grid item marginRight={2} marginBottom={5}>
+          <TextField type="text" label="other" id="other" fullWidth />
+        </Grid>
+        <Grid item>
+          <Button variant="outlined" onClick={handleClick}>
+            click me
+          </Button>
         </Grid>
       </Container>
     </ThemeProvider>
