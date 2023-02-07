d40e3c051c2077167357f504248efb89cd6829c8
mandy6720 <amanda.hewitt01@gmail.com>
2023-02-07 11:59:23 -0600

    ?

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -43,7 +43,7 @@ const Footer = () => {
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3434.480057694476!2d-97.96537968451189!3d30.592219981686245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x865b26265c4bab9f%3A0x12d1d00952203b6d!2s15101%20Chaparral%20Dr%2C%20Leander%2C%20TX%2078641!5e0!3m2!1sen!2sus!4v1673614148879!5m2!1sen!2sus"
           width="450"
           height="300"
-          sx={{ border: 0, right: 0 }}
+          sx={{ border: 0, right: 0, position: "absolute" }}
           allowFullScreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
